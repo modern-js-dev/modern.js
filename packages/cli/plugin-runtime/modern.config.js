3346a0e5b7b5b5625a7f540d4b69ba425671a4fc
caohuilin <caohuilin@bytedance.com>
2022-07-26 13:52:15 +0800

    feat: bundle runtime

--- packages/cli/plugin-runtime/modern.config.js
+++ packages/cli/plugin-runtime/modern.config.js
@@ -1,2 +1,25 @@
 /** @type {import('@modern-js/module-tools').UserConfig} */
-module.exports = {};
+module.exports = {
+  output: {
+    buildConfig: [
+      {
+        buildType: 'bundle',
+        format: 'cjs',
+        // enableDts: true,
+        bundleOptions: {
+          skipDeps: false,
+          externals: ['@modern-js/core'],
+        },
+      },
+      {
+        buildType: 'bundle',
+        format: 'esm',
+        // enableDts: true,
+        bundleOptions: {
+          skipDeps: false,
+          externals: ['@modern-js/core'],
+        },
+      },
+    ],
+  },
+};
