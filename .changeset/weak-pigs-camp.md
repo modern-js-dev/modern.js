37bd0070f6064f32807b22bc64447d97ef853b1b
caohuilin <caohuilin@bytedance.com>
2022-07-28 15:51:05 +0800

    chore(monorepo-tools): bundle `@modern-js/monorepo-tools`

---
+++ .changeset/weak-pigs-camp.md
@@ -0,0 +1,7 @@
+---
+'@modern-js/monorepo-tools': minor
+---
+
+chore(monorepo-tools): bundle `@modern-js/monorepo-tools`
+
+chore(monorepo-tools): 打包 `@modern-js/monorepo-tools`
--- packages/solutions/monorepo-tools/modern.config.js
+++ packages/solutions/monorepo-tools/modern.config.js
@@ -1,2 +1,22 @@
+import pkgJSON from './package.json';
+
 /** @type {import('@modern-js/module-tools').UserConfig} */
-module.exports = {};
+module.exports = {
+  output: {
+    buildConfig: [
+      {
+        enableDts: true,
+        dtsOnly: true,
+        outputPath: './types',
+      },
+      {
+        buildType: 'bundle',
+        format: 'cjs',
+        bundleOptions: {
+          skipDeps: false,
+          externals: [...Object.keys(pkgJSON.dependencies), 'spawn-sync'],
+        },
+      },
+    ],
+  },
+};
